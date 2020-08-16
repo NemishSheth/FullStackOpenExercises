5012ec41a24cf9794dd7929b90d49b013f8b5720
Nemish Sheth <170020107054ait@gmail.com>
2020-08-16 16:28:02 +0530

    ex1.12 -ex1.14 added

--- part1/ex1.14/src/index.js
+++ part1/ex1.14/src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React,{useState} from 'react'
+import ReactDOM from 'react-dom'
 
+const App = (props) =>{
+  const [selected,setSelected] = useState(0)
+  const [stVotes,setVotes] = useState(props.votes)
+
+  const nxtAnc = () =>{
+    let rand = Math.floor(Math.random() * (props.anecdotes.length));
+    setSelected(rand)
+  }
+  const addVote = () =>{
+    let nv = [...stVotes]
+    nv[selected]+=1
+    setVotes(nv)
+  }
+
+  let mx = 0;
+
+  for(let x=0;x<stVotes.length;x++){
+    if(stVotes[x] > stVotes[mx])
+      mx = x
+  }
+
+  return(
+    <div>
+     
+      <p>{props.anecdotes[selected]} <br />
+       has {stVotes[selected]} votes</p>
+     
+      <button onClick={addVote}>Vote</button>
+      <button onClick={nxtAnc}>Next anecdotes</button>
+     
+      <h1>Anecdotes with most votes</h1>
+     
+      <p>{props.anecdotes[mx]} <br />
+       has {stVotes[mx]} votes</p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const votes = new Array(anecdotes.length+1).join("0").split("").map(parseFloat)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} votes={votes}/>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
